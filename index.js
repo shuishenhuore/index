834337e24dd011e6c029b3eee0e0235aeb015aaa
shuishenhuore <143575676+shuishenhuore@users.noreply.github.com>
2024-09-18 08:55:06 +0800

    Update index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var h1 = document.getElementById('h1');
+    var oLo = document.getElementById('lo')
+    h1.onclick = function(){
+        alert('你好呀！');
+    }
+    lo.onclick = function(){
+        lo.src = 'image/爱心2.png';
+    }
+}
